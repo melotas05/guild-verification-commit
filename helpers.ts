4a50484b5dc586063e675f15b3dc7f8a392137b2
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-01 20:14:56 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:14:36.269Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-01T17:14:56.544Z
+Commit: Fix CSS styling bug on mobile devices
