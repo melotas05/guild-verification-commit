83b68cd75b220ee7b68301c5c9f240b6bf1222ab
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:47:18 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:47:04.115Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T01:47:17.517Z
+Commit: Fix broken links in documentation
