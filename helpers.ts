543bc5bdf41ab7535068e40b9343a98a2acff960
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:39:00 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:38:41.570Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T01:38:58.902Z
+Commit: Introduce real-time notifications
