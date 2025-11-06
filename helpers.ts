ba4396cd056b817524322c18dbea1086c405affd
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:40:17 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:39:54.405Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T01:40:15.893Z
+Commit: Fix CSS styling bug on mobile devices
