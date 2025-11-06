041d1a762b2dbb5e773be6528fba46551d77b8f3
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:38:46 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:38:27.058Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T01:38:45.251Z
+Commit: Fix broken links in documentation
