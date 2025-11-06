fd6054d57b53b150b27ebb358daa8f876e3e29e0
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:39:07 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:38:52.344Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:39:05.889Z
+Commit: Patch API endpoint error handling
