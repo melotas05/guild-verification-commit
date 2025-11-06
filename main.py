d4b34a22b789f7fdf6af72a3163370bccbeb65d8
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:49:48 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:49:38.986Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-06T01:49:46.990Z
+Commit: Patch API endpoint error handling
