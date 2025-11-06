9e5cb871dc940d44c3c214995724c113cfb48333
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:39:31 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:39:27.546Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T01:39:30.107Z
+Commit: Fix memory leak in event handlers
