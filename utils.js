90027622513716902f4b032b236d8f51ee19e63b
bernoo <113217918+melotas05@users.noreply.github.com>
2025-11-06 04:39:23 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:39:05.889Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:39:22.427Z
+Commit: Fix memory leak in event handlers
